aaac0f55ffbb18d50a5d3f784b83ccdd46ae6e5e
Djonahuti <djonah04@gmail.com>
2025-11-02 20:18:27 +0100

    commit

--- src/components/mail/Sidebar.tsx
+++ src/components/mail/Sidebar.tsx
@@ -258,13 +258,16 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible="icon" className="overflow-hidden [&>[data-sidebar=sidebar]]:flex-row" {...props}>
       {/* Icon Sidebar */}
-      <Sidebar collapsible="none" className="!w-[calc(var(--sidebar-width-icon)_+_1px)] border-r bg-gray-50 dark:bg-gray-900/90">
+      <Sidebar
+       collapsible="none" 
+       className="!w-[calc(var(--sidebar-width-icon)_+_1px)] border-r sm:ring-1 sm:ring-gray-900/10 border-gray-200 dark:border-gray-600 bg-gray-50 dark:bg-gray-900/90"
+      >
         <SidebarHeader>
           <SidebarMenu>
             <SidebarMenuItem>
-              <SidebarMenuButton size="lg" asChild>
+              <SidebarMenuButton size="lg" asChild className="md:h-8 md:p-0">
                 <a href={getHomeLink()}>
-                  <div className="flex aspect-square size-8 items-center justify-center">
+                  <div className="flex aspect-square size-8 items-center justify-center text-sidebar-primary-foreground">
                     <img src={logoUrl} alt="logo" width={85} height={20} />
                   </div>
                 </a>
@@ -274,7 +277,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         </SidebarHeader>
         <SidebarContent>
           <SidebarGroup>
-            <SidebarGroupContent className="px-1.5">
+            <SidebarGroupContent className="px-1.5 md:px-0">
               <SidebarMenu>
                 {data.navMain.map((item) => (
                   <SidebarMenuItem key={item.title}>
@@ -289,7 +292,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                       className={`px-2.5 transition-colors ${
                         activeItem?.title === item.title
                           ? 'bg-red-50 text-primary border border-primary'
-                          : 'text-gray-700 dark:text-gray-200 hover:bg-primary hover:text-white'
+                          : 'text-gray-700 dark:text-gray-200 hover:bg-primary hover:text-white transition-colors'
                       }`}
                     >
                       <item.icon />
@@ -310,7 +313,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </Sidebar>
 
       {/* Main Sidebar */}
-      <Sidebar collapsible="none" className="hidden flex-1 md:flex flex-col bg-gray-50 dark:bg-gray-900/90">
+      <Sidebar collapsible="none" className="hidden flex-1 md:flex flex-col sm:ring-1 sm:ring-gray-900/10 border-b border-gray-200 dark:border-gray-600 bg-gray-50 dark:bg-gray-900/90">
         <SidebarHeader className="gap-3.5 border-b p-4">
           <div className="flex w-full items-center justify-between">
             <div className="text-base font-medium text-foreground">{activeItem?.title}</div>

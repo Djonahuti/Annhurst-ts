6fd24846e16e97c7d11c6a8a92df690fe39e0643
Djonahuti <djonah04@gmail.com>
2025-11-12 16:54:25 +0100

    commit

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -256,7 +256,7 @@ export default function AboutPage() {
             </motion.div>
           </div>
         </div>
-      </AnimatedSection>
+      </AnimatedSection> 
 
       {/* Values */}
       <AnimatedSection>
